2c7457471c6974f47f08fcf3f57d4f7dacff96c1
ruwatana <ruwatana@gmail.com>
2021-01-11 14:31:32 +0900

    :tada: Initial Commit

---
+++ src/main/java/com/example/graphqlclientspring/GraphqlClientSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlclientspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlClientSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlClientSpringApplication.class, args);
+	}
+
+}
